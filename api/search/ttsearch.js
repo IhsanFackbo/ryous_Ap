7fb526073773174a7ec55aa5085bcec366e42f69
IhsanFackbo <159555124+IhsanFackbo@users.noreply.github.com>
2025-11-01 23:49:05 +0700

    Create search/ttsearch.js

---
+++ api/search/ttsearch.js
@@ -0,0 +1,30 @@
+// api/downloader/tiktok-search.js
+const src = require('../../lib/scrape_file/search/ttsearch');
+
+let handler = async (res, req) => {
+  try {
+    const { text } = req.query;
+    const q = text?.trim() || 'jedag jedug anime'; // default jika kosong
+    const result = await src.tiktokSearch(q);
+
+    res.reply({
+      success: true,
+      keyword: q,
+      count: result.length,
+      result
+    });
+  } catch (error) {
+    res.reply(
+      { success: false, error: error.message },
+      { code: 500 }
+    );
+  }
+};
+
+handler.alias = 'TikTok Search';
+handler.category = 'Downloader';
+handler.params = {
+  text: { desc: 'Kata kunci untuk pencarian video TikTok.', example: 'anime edit jedag jedug' }
+};
+
+module.exports = handler;
