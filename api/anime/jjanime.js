d1fb013ec92ddc16db760934358e486747ad25de
IhsanFackbo <159555124+IhsanFackbo@users.noreply.github.com>
2025-11-01 23:28:31 +0700

    Update jjanime.js

--- api/anime/jjanime.js
+++ api/anime/jjanime.js
@@ -4,13 +4,11 @@ const src = require('../../lib/scrape_file/anime/jjanime');
 let handler = async (res, req) => {
   try {
     const result = await src.randomJJAnime();
-
-    // kirim hasil langsung (tidak perlu query)
     res.reply({
       success: true,
       keyword: 'jedag jedug anime',
-      total: result.length,
-      result,
+      count: result.length,
+      result
     });
   } catch (error) {
     res.reply({ success: false, error: error.message }, { code: 500 });
@@ -19,6 +17,6 @@ let handler = async (res, req) => {
 
 handler.alias = 'JJ Anime Random';
 handler.category = 'Anime';
-handler.params = {}; // tidak butuh params
+handler.params = {}; // tidak butuh input
 
 module.exports = handler;
