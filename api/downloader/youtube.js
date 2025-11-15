38efd632a20ab1538421a27e476494cad1546d0a
IhsanFackbo <159555124+IhsanFackbo@users.noreply.github.com>
2025-11-15 22:23:41 +0700

    Update youtube.js

--- api/downloader/youtube.js
+++ api/downloader/youtube.js
@@ -1,23 +1,33 @@
 const src = require('../../lib/scrape_file/downloader/savetube');
 
-let handler = async (res, req) => {
-    try {
-        const { url, format } = req.query;
-        if (!/youtube.com|youtu.be/.test(url)) return res.reply('Invalid url.', { code: 400 });
-        
-        const result = await src(url, format);
-        res.reply(result);
-    } catch (error) {
-        res.reply(error.message, { code: 500 });
+let handler = async (req, res) => {
+  try {
+    const { url, format = '360p' } = req.query;
+
+    if (!url || !/(youtube\.com|youtu\.be)/.test(url)) {
+      return res.reply('Invalid url.', { code: 400 });
     }
+
+    const result = await src(url, format);
+
+    return res.reply(result);
+  } catch (error) {
+    return res.reply(error.message, { code: 500 });
+  }
 };
 
 handler.alias = 'YouTube Downloader';
 handler.category = 'Downloader';
-handler.status = 'error'; 
+handler.status = 'active';
 handler.params = {
-    url: { desc: 'Input url from youtube.', example: 'https://youtube.com/...' },
-    format: { desc: 'Input format.', options: ['144', '240', '360', '480', '720', '1080', 'mp3'] }
+  url: {
+    desc: 'Input url from youtube.',
+    example: 'https://youtu.be/xxxxxx'
+  },
+  format: {
+    desc: 'Choose format.',
+    options: ['128k', '320k', '144p', '240p', '360p', '720p', '1080p']
+  }
 };
 
 module.exports = handler;
